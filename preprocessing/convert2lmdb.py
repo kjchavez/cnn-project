fdef644ba69d9785b0c8b324bda6d608edefb007
Kevin Chavez <kjchavez@stanford.edu>
2015-02-23 10:55:17 -0800

    added preprocessing utility to convert videos to lmdb database

---
+++ preprocessing/convert2lmdb.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Converts AVI files to fixed length raw pixel data stored in an LMDB database.
+Created on Sun Feb 22 12:16:17 2015
+
+@author: Kevin Chavez
+"""
+import os
+import lmdb
+import cv2
+from cv2.cv import * #Constants of the form CV_*
+import argparse
+import numpy as np
+import itertools
+from src.dataio.datum import Datum4D
+
+APPROXIMATE_MEAN = 127
+MAX_ATTEMPTS = 3
+
+def read_clip(capture, video_filename, num_frames, 
+              height=-1, width=-1, start_frame=0, mean_subtract=True):
+    success = capture.open(video_filename)
+    if not success:
+        print "Couldn't open video"
+        print "Crashed at index %d." % index
+        return None
+        
+    if width < 0:
+        width = capture.get(CV_CAP_PROP_FRAME_WIDTH)
+    if height < 0:
+        height = capture.get(CV_CAP_PROP_FRAME_HEIGHT)
+    
+    top = (capture.get(CV_CAP_PROP_FRAME_HEIGHT) - height)/2
+    bottom = top + height
+    left = (capture.get(CV_CAP_PROP_FRAME_WIDTH) - width)/2
+    right = left + width
+
+    clip = np.empty((3,num_frames,height,width),dtype=np.uint8)
+    
+    if start_frame > 0:
+        capture.set(CV_CAP_PROP_POS_FRAMES,start_frame)
+        
+    for i in xrange(num_frames):
+        frame_available, frame = capture.read()
+        if not frame_available:
+            print "Ran out of frames when reading", video_filename
+            print "Padding with %d empty frames." % (num_frames-i)
+            clip[:,i:] = 0
+            break
+            
+        if mean_subtract:
+            clip[:,i] = frame[top:bottom, left:right, :].transpose(2,0,1) \
+                        - APPROXIMATE_MEAN
+        else:
+            clip[:,i] = frame[top:bottom, left:right, :].transpose(2,0,1)
+
+    return clip
+    
+def create_datum(clip,label):
+    """ Creates a Datum structure from a 4D clip and label. """
+    datum = Datum4D.array_to_datum(clip,label)
+    return datum
+    
+def write_to_lmdb(env, keys, data):
+    """ Adds a 4D video clip to an LMDB database using Caffe's Datum structure.
+    
+    Args:
+        env (lmdb.Environment): database environment
+        data ([Datum]): list of datum object to be written to database
+
+    Returns:
+        Boolean flag indicating success or failure.
+    """
+    try:
+        with env.begin(write=True) as txn:
+            cursor = txn.cursor()
+            for key,datum in itertools.izip(keys,data):
+                cursor.put(key,datum.value)
+        return True
+    except:
+        return False
+        
+        
+def convert_list(list_file,database_name,root_directory,
+                 num_frames=16,width=-1,height=-1,start_frame=0,batch_size=10,
+                 map_size=100e6):
+
+    capture = cv2.VideoCapture()
+    env = lmdb.open(database_name,map_size=map_size,writemap=True)
+    data = []
+    keys = []
+    done = False
+    batch_num = 1
+    curr_key = 0
+    with open(list_file) as video_list:
+        while not done:
+            for k in xrange(batch_size):
+                try:
+                    line = next(video_list)
+                except StopIteration:
+                    done = True
+                    break
+                
+                filename, label = line.split()
+                print "Processing", filename, "..."                
+                
+                label = int(label)
+                full_filename = os.path.join(root_directory,filename)                
+                clip = read_clip(capture,full_filename,num_frames,height=height,
+                                 width=width,start_frame=start_frame)
+                datum = create_datum(clip,label)
+                data.append(datum)
+                keys.append("%08d_%s" % (curr_key,filename))
+                curr_key += 1
+
+            for n in range(MAX_ATTEMPTS):
+                success = write_to_lmdb(env,keys,data)
+                if success:
+                    break
+            else:
+                print "Warning: could not write batch %d to database" % batch_num
+            
+            data = []
+            batch_num += 1
+            
+    env.close()
+        
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file")
+    parser.add_argument("root",help="Root directory of video files specified in"
+                                     " input file")
+    parser.add_argument("database_name")
+    parser.add_argument("--length", "-l", type=int, default=16,
+                        help="Number of frames per video")
+    parser.add_argument("--width", "-w", type=int, default=-1, 
+                        help="Fixed width to crop to (default is no cropping)")
+    parser.add_argument("--height", "-g", type=int, default=-1,
+                        help="Fixed height to crop to (default is no cropping)")
+    parser.add_argument("--batchsize","-b",type=int,default=10,
+                        help="Number of videos to write to lmdb at a time"
+                             " (note: all of these will have to be in memory,"
+                             " so make sure you have space for it).")
+    parser.add_argument("--mapsize","-m",type=int,default=100e6,
+                        help="Maximum size of lmdb database")
+
+    args = parser.parse_args()
+    
+    convert_list(args.input_file,args.database_name,args.root,
+                 num_frames=args.length,width=args.width,height=args.height,
+                 start_frame=0,batch_size=args.batchsize,map_size=args.mapsize)
+
+if __name__ == "__main__":
+    main()
