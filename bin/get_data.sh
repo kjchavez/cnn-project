c3dbbe6fb4bc0c0cbe23d12e2cb4166191ac00e5
Kevin Chavez <kjchavez@stanford.edu>
2015-02-27 14:03:59 -0800

    data setup utils

---
+++ bin/get_data.sh
@@ -0,0 +1,8 @@
+cd data
+wget http://crcv.ucf.edu/data/UCF101/UCF101.rar
+unrar x UCF101.rar
+wget http://crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip
+unzip UCF101TrainTestSplits-RecognitionTask.zip
+rm *.zip
+rm *.rar
+cd ..
