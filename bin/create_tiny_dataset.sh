623e71e6bdee41a280fdee657afd1e1762b02a9d
Kevin Chavez <kjchavez@stanford.edu>
2015-03-01 13:17:10 -0800

    create_tiny_dataset complete

--- bin/create_tiny_dataset.sh
+++ bin/create_tiny_dataset.sh
@@ -1,3 +1,5 @@
-python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/tinytrain.txt data/UCF-101 data/tinytraindb.lmdb --randomize --batchsize 25 --mapsize 1000000000
+head -10 data/ucfTrainTestlist/trainlist01.txt > data/ucfTrainTestlist/tinytrain.txt
+head -10 data/ucfTrainTestlist/trainlist02.txt > data/ucfTrainTestlist/tinyval.txt
 
+python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/tinytrain.txt data/UCF-101 data/tinytraindb.lmdb --randomize --batchsize 25 --mapsize 1000000000
 python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/tinyval.txt data/UCF-101 data/tinyvaldb.lmdb --randomize --batchsize 25 --mapsize 1000000000
