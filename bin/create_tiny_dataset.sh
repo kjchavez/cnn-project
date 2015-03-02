41ccb8c041a6433225270cbdbc5baa1341c84039
Kevin Chavez <kjchavez@stanford.edu>
2015-03-01 16:47:44 -0800

    more realistic tiny dataset, with a random spread of classes

--- bin/create_tiny_dataset.sh
+++ bin/create_tiny_dataset.sh
@@ -1,5 +1,7 @@
-head -10 data/ucfTrainTestlist/trainlist01.txt > data/ucfTrainTestlist/tinytrain.txt
-head -10 data/ucfTrainTestlist/trainlist02.txt > data/ucfTrainTestlist/tinyval.txt
+shuf data/ucfTrainTestlist/trainlist01.txt > shuffled.txt
+head -20 shuffled.txt > data/ucfTrainTestlist/tinytrain.txt
+head -30 shuffled.txt | tail -10 > data/ucfTrainTestlist/tinyval.txt
+rm shuffled.txt
 
 python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/tinytrain.txt data/UCF-101 data/tinytraindb.lmdb --randomize --batchsize 25 --mapsize 1000000000
 python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/tinyval.txt data/UCF-101 data/tinyvaldb.lmdb --randomize --batchsize 25 --mapsize 1000000000
