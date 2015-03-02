38fe8127b7a40fb409c15270a81cc9d00c3cd99e
Kevin Chavez <kjchavez@stanford.edu>
2015-03-01 16:29:56 -0800

    create_dataset now shuffles and splits train into train/val splits

--- bin/create_dataset.sh
+++ bin/create_dataset.sh
@@ -1,3 +1,10 @@
-python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/trainlist01.txt data/UCF-101 data/traindb.lmdb --randomize --batchsize 25 --mapsize 300000000000
+#!/bin/bash
 
-python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/testlist01.txt data/UCF-101 data/testdb.lmdb --randomize --batchsize 25 --mapsize 150000000000
+# Split training set into train and validation sets
+shuf data/ucfTrainTestlist/trainlist01.txt > shuffled.txt
+head -8000 shuffled.txt > train.txt
+tail shuffled.txt --lines=+8001 > val.txt
+
+python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/train.txt data/UCF-101 data/traindb.lmdb --batchsize 25 --mapsize 300000000000
+
+python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/val.txt data/UCF-101 data/valdb.lmdb --batchsize 25 --mapsize 150000000000
