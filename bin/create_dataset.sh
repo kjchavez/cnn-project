1bc8bbee38bbd5522361571213333f37685e7b15
Kevin Chavez <kjchavez@stanford.edu>
2015-03-01 16:34:30 -0800

    fixed paths in create_dataset

--- bin/create_dataset.sh
+++ bin/create_dataset.sh
@@ -2,9 +2,10 @@
 
 # Split training set into train and validation sets
 shuf data/ucfTrainTestlist/trainlist01.txt > shuffled.txt
-head -8000 shuffled.txt > train.txt
-tail shuffled.txt --lines=+8001 > val.txt
+head -8500 shuffled.txt > data/ucfTrainTestlist/train.txt
+tail shuffled.txt --lines=+8501 > data/ucfTrainTestlist/val.txt
+rm shuffled.txt
 
 python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/train.txt data/UCF-101 data/traindb.lmdb --batchsize 25 --mapsize 300000000000
 
-python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/val.txt data/UCF-101 data/valdb.lmdb --batchsize 25 --mapsize 150000000000
+python -m src.preprocessing.convert2lmdb data/ucfTrainTestlist/val.txt data/UCF-101 data/valdb.lmdb --batchsize 25 --mapsize 100000000000
