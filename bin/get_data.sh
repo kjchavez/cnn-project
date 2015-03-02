02aec4de2cef9d975ad7318a84bc18ca41409778
Kevin Chavez <kevin.j.chavez@gmail.com>
2015-03-02 10:24:40 -0500

    running smallnet experiment with this commit

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -3,6 +3,4 @@ wget http://crcv.ucf.edu/data/UCF101/UCF101.rar
 unrar x UCF101.rar
 wget http://crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip
 unzip UCF101TrainTestSplits-RecognitionTask.zip
-rm *.zip
-rm *.rar
 cd ..
--- experiments.py
+++ experiments.py
@@ -10,25 +10,25 @@ from src.convnet3d.cnn3d import ConvNet3D
 from src.convnet3d.solver import Solver
 
 video_shape = (16,240,320)
-batch_size = 10
+batch_size = 4
 seed = 1234
 
 # Baseline A: Small CNN
 smallnet = ConvNet3D("small-net",video_shape,batch_size,seed=seed)
 smallnet.add_train_data("data/traindb.lmdb")
 smallnet.add_val_data("data/valdb.lmdb")
-smallnet.add_conv_layer("conv1",(3,3,3),4)
+smallnet.add_conv_layer("conv1",(5,7,7),8)
 smallnet.add_pool_layer("pool1",(2,2,2))
-smallnet.add_conv_layer("conv2",(3,3,3),4)
+smallnet.add_conv_layer("conv2",(3,3,3),16)
 smallnet.add_pool_layer("pool2",(2,2,2))
-smallnet.add_conv_layer("conv3",(3,3,3),4)
+smallnet.add_conv_layer("conv3",(3,3,3),16)
 smallnet.add_pool_layer("pool3",(2,2,2))
-smallnet.add_conv_layer("conv4",(3,3,3),4)
+smallnet.add_conv_layer("conv4",(3,3,3),16)
 smallnet.add_pool_layer("pool4",(2,2,2))
-smallnet.add_fc_layer("fc1",8,0.5)
+smallnet.add_fc_layer("fc1",128,0.5)
 smallnet.add_softmax_layer("softmax",101)
 
-reg = 1e-2
+reg = 5e-3
 reg_params = {
     "conv1_W": reg,
     "conv2_W": reg,
@@ -38,14 +38,14 @@ reg_params = {
     "softmax_W": reg}
 
 lr_params = {
-    "rate": 1e-8,
+    "rate": 1e-9,
     "decay": 0.95,
-    "step": 500}
+    "step": 2000}
 
 rmsprop_decay = 0.99
 snapshot_params = {
     "dir": "models/smallnet",
-    "rate": 2}
+    "rate": 500}
 
 solver = Solver(smallnet,reg_params,lr_params,rmsprop_decay)
-solver.train(100,snapshot_params,validate_rate=2,loss_rate=1)
+solver.train(20000,snapshot_params,validate_rate=500,loss_rate=1)
