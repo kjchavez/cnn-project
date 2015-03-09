05981c0cf45472fe010d8c3cfac5b9e0f0a16c2e
Kevin Chavez <kevin.j.chavez@gmail.com>
2015-03-08 23:35:07 -0400

    Fixed a few bugs in hpsearch

--- hpsearch.py
+++ hpsearch.py
@@ -17,7 +17,7 @@ parser.add_argument("--device",default=None,
                     help="Specify a particular GPU or CPU. Note to specify a "
                          "particular GPU, should invoke script with THEANO_FLAGS "
                          "=device=cpu.")
-parser.add_argument("--trial-id",dest="trial_index",type=int,default=0,
+parser.add_argument("--trial-id",dest="trial_id",type=int,default=0,
                     help="ID for first trial, rest will be sequential")
 parser.add_argument("--num-trials","-n",dest="num_trials",type=int,default=10,
                     help="Number of trials of random search to run")
@@ -47,19 +47,26 @@ kwargs = {
 # Searching on parameters:
 # learning_rate, reg, dropout
 for n in xrange(args.num_trials):
-    kwargs['lr'] = np.float32(10**np.random.uniform(-8,-2))
+    kwargs['lr'] = np.float32(10**np.random.uniform(-8,-3))
     kwargs['reg'] = np.float32(10**np.random.uniform(-8,1))
     kwargs['dropout'] = [np.float32(np.random.choice([0.2,0.4,0.6,0.8]))]
 
     if args.optflow:
         kwargs['optflow_weight'] = np.float32(10**np.random.uniform(-2,0))
-    
-    print "Starting trial with lr %0.4e, reg %0.4e, dropout %0.2f..." % \
-          (kwargs['lr'], kwargs['reg'], kwargs['dropout'][0])
+        print "Starting trial with lr %0.4e, reg %0.4e, dropout %0.2f, optflow-reg %0.3e" % \
+              (kwargs['lr'], kwargs['reg'], kwargs['dropout'][0],kwargs['optflow_weight'])
+    else:
+        print "Starting trial with lr %0.4e, reg %0.4e, dropout %0.2f..." % \
+              (kwargs['lr'], kwargs['reg'], kwargs['dropout'][0])
           
     def log_result(best_val_acc,best_val_iter):
         with open(logfilename,'a') as fp:
-            print >> fp, "%03d\t%0.4e\t%0.4e\t%s\t%0.4f\t%06d" % \
+            if args.optflow:
+                print >> fp, "%03d\t%0.4e\t%0.4e\t%s\t%0.3e\t%0.4f\t%06d" % \
+                         (n, kwargs['lr'], kwargs['reg'], str(kwargs['dropout']), 
+                          kwargs['optflow_weight'],best_val_acc, best_val_iter)
+            else:
+                print >> fp, "%03d\t%0.4e\t%0.4e\t%s\t%0.4f\t%06d" % \
                          (n, kwargs['lr'], kwargs['reg'], str(kwargs['dropout']), 
                           best_val_acc, best_val_iter)
         print "Completed trial %d." % n
