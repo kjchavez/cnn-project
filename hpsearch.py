5518d66e4237c7e6e86d5e775c7c485e54f4fe88
Kevin Chavez <kjchavez@stanford.edu>
2015-03-11 05:35:02 -0700

    Equipped hpsearch for consistent optflow experiments

--- hpsearch.py
+++ hpsearch.py
@@ -7,6 +7,7 @@ Created on Fri Mar  6 00:12:39 2015
 @author: Kevin Chavez
 """
 import os
+import shutil
 import argparse
 import numpy as np
 
@@ -43,8 +44,8 @@ kwargs = {
     'mom_final' : 0.9,
     'mom_step' : 0.1,
     'num_iter' : args.num_iter,
-    'snapshot_rate' : 1000,
-    'validate_rate' : 1000
+    'snapshot_rate' : 500,
+    'validate_rate' : 500
 }
 
 # Searching on parameters:
@@ -55,33 +56,52 @@ for n in xrange(args.trial_id,args.trial_id+args.num_trials):
     kwargs['dropout'] = [np.float32(np.random.choice([0.1,0.2,0.3,0.4]))]
 
     if args.optflow:
-        kwargs['optflow_weight'] = np.float32(10**np.random.uniform(-2,0))
+        # Run optflow experiments
+        optflow_weight = np.float32(10**np.random.uniform(-2,0))
         print "Starting trial with lr %0.4e, reg %0.4e, dropout %0.2f, optflow-reg %0.3e" % \
-              (kwargs['lr'], kwargs['reg'], kwargs['dropout'][0],kwargs['optflow_weight'])
+              (kwargs['lr'], kwargs['reg'], kwargs['dropout'][0], optflow_weight)
+              
+        def log_result(best_val_acc,best_val_iter,optflow_acc,optflow_iter):
+            if best_val_iter is None:
+                best_val_iter = -1
+            if optflow_iter is None:
+                optflow_iter = -1
+            with open(logfilename,'a') as fp:
+                print >> fp, "%03d\t%0.4e\t%0.4e\t%s\t%0.3e\t%0.4f\t%06d\t%0.4f\t%06d" % \
+                         (n, kwargs['lr'], kwargs['reg'], str(kwargs['dropout']), 
+                          kwargs['optflow_weight'],best_val_acc, best_val_iter,
+                          optflow_acc, optflow_iter)
+                              
+            print "Completed trial %d." % n
+
+        val_acc, val_iter = train(net_file, n, **kwargs.copy())
+        kwargs['optflow_weight'] = optflow_weight
+        optflow_acc, optflow_iter = train(net_file, n + 1000,**kwargs.copy())
+
+        
+        log_result(val_acc,val_iter,optflow_acc,optflow_iter)
+
+              
     else:
+        # Just regular hyperparameter searching
         print "Starting trial with lr %0.4e, reg %0.4e, dropout %0.2f..." % \
               (kwargs['lr'], kwargs['reg'], kwargs['dropout'][0])
           
-    def log_result(best_val_acc,best_val_iter):
-        with open(logfilename,'a') as fp:
-            if args.optflow:
-                print >> fp, "%03d\t%0.4e\t%0.4e\t%s\t%0.3e\t%0.4f\t%06d" % \
-                         (n, kwargs['lr'], kwargs['reg'], str(kwargs['dropout']), 
-                          kwargs['optflow_weight'],best_val_acc, best_val_iter)
-            else:
+        def log_result(best_val_acc,best_val_iter):
+            with open(logfilename,'a') as fp:
                 print >> fp, "%03d\t%0.4e\t%0.4e\t%s\t%0.4f\t%06d" % \
                          (n, kwargs['lr'], kwargs['reg'], str(kwargs['dropout']), 
                           best_val_acc, best_val_iter)
-        print "Completed trial %d." % n
-
-    while True:
-        try:                    
-	    val_acc, val_iter = train(net_file, n, **kwargs.copy())
-        except:
-	    print "Train function failed. Restarting..."
-            os.rmdir("results/%s-%04d" % (net_file.split('/')[1].split('.')[0],n))
-            continue
-
-        # Once successful, log results
-        log_result(val_acc,val_iter)
-        break
+            print "Completed trial %d." % n
+    
+        while True:
+            try:                    
+                val_acc, val_iter = train(net_file, n, **kwargs.copy())
+            except:
+                print "Train function failed. Restarting..."
+                shutil.rmtree("results/%s-%04d" % (net_file.split('/')[1].split('.')[0],n))
+                continue
+            
+            # Once successful, log results
+            log_result(val_acc,val_iter)
+            break
